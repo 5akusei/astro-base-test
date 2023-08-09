272cd34787478352f037ee214b4c66125f9b6bf6
5akusei <jeisonmulti@gmail.com>
2023-08-08 19:59:32 -0600

    astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://5akusei.github.io',
+  base: '/astro-base-test'
 });
